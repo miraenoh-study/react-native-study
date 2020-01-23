872cfc8e962d41fc6f8c4f59a7958c003b8d7b7e
miraenoh <mnoh@novamobility.com>
2020-01-23 12:36:51 -0800

    Removed unnecessary imports

--- 2-guess-a-number/App.js
+++ 2-guess-a-number/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Header from './src/components/Header';
 import StartGameScreen from './src/screens/StartGameScreen.js';
