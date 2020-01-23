313056ed362354310cd3ee331806b025798781d0
miraenoh <mnoh@novamobility.com>
2020-01-23 11:47:19 -0800

    Added a gamescreen with basic inputs

--- 2-guess-a-number/App.js
+++ 2-guess-a-number/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Header from "./src/components/Header";
+import StartGameScreen from "./src/screens/StartGameScreen.js";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess A Number" />
+      <StartGameScreen />
     </View>
   );
 }
