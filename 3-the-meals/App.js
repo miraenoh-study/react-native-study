decd9098575aaa5dc15b210466d46ae4807dcb35
miraenoh <mnoh@novamobility.com>
2020-02-25 16:18:14 -0800

    Finished the basic setup with screen files & fonts

--- 3-the-meals/App.js
+++ 3-the-meals/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    sf: require('./assets/fonts/SFNSDisplay-Light.ttf'),
+    'sf-bold': require('./assets/fonts/SFNSDisplay-Medium.ttf')
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />;
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
