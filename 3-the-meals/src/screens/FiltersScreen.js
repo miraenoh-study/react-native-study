6f46d4a1868903753cbbd68488f73e2870357db1
miraenoh <mnoh@novamobility.com>
2020-03-13 11:59:25 -0700

    Added the filtering logic

--- 3-the-meals/src/screens/FiltersScreen.js
+++ 3-the-meals/src/screens/FiltersScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import DrawerButton from '../components/DrawerButton';
 import CustomHeaderButton from '../components/HeaderButton';
+import { setFilters } from '../store/actions/meals';
 
 import { Colors } from '../constants';
 
@@ -30,6 +32,8 @@ const FiltersScreen = props => {
   const [isVegan, setIsVegan] = useState(false);
   const [isVegetarian, SetIsVegetarian] = useState(false);
 
+  const dispatch = useDispatch();
+
   const saveFilters = useCallback(() => {
     const appliedFilters = {
       glutenFree: isGlutenFree,
@@ -38,8 +42,8 @@ const FiltersScreen = props => {
       vegetarian: isVegetarian
     };
 
-    console.log(appliedFilters);
-  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+    dispatch(setFilters(appliedFilters));
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian, dispatch]);
 
   // We have to avoid 'navigation' from the dependencies, because it causes the unlimited loop.
   // navigation.setParams() causes the change to 'navigation', so it keeps calling the useEffect.
@@ -51,18 +55,22 @@ const FiltersScreen = props => {
     <View style={styles.screen}>
       <Text style={styles.title}>Available Filters / Restrictions</Text>
       <FilterSwitch
-        label='Gluten-free'
+        label="Gluten-free"
         state={isGlutenFree}
         onChange={newValue => setIsGlutenFree(newValue)}
       />
       <FilterSwitch
-        label='Lactose-free'
+        label="Lactose-free"
         state={isLactoseFree}
         onChange={newValue => setIsLactoseFree(newValue)}
       />
-      <FilterSwitch label='Vegan' state={isVegan} onChange={newValue => setIsVegan(newValue)} />
       <FilterSwitch
-        label='Vegetarian'
+        label="Vegan"
+        state={isVegan}
+        onChange={newValue => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
         state={isVegetarian}
         onChange={newValue => SetIsVegetarian(newValue)}
       />
@@ -76,7 +84,11 @@ FiltersScreen.navigationOptions = navData => {
     headerLeft: () => <DrawerButton navigation={navData.navigation} />,
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-        <Item title='Save' iconName='ios-save' onPress={navData.navigation.getParam('save')} />
+        <Item
+          title="Save"
+          iconName="ios-save"
+          onPress={navData.navigation.getParam('save')}
+        />
       </HeaderButtons>
     )
   };
--- 3-the-meals/src/store/reducers/meals.js
+++ 3-the-meals/src/store/reducers/meals.js
@@ -1,5 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/meals';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/meals';
 
 const initialState = {
   meals: MEALS,
@@ -12,7 +12,9 @@ const mealsReducer = (state = initialState, action) => {
     //  If favorite -> delete from favorites / if not -> add into favorites
     case TOGGLE_FAVORITE:
       //  if exists -> existingIndex >= 0. Otherwise, existingIndex = -1
-      const existingIndex = state.favoriteMeals.findIndex(meal => meal.id === action.mealId);
+      const existingIndex = state.favoriteMeals.findIndex(
+        meal => meal.id === action.mealId
+      );
       if (existingIndex >= 0) {
         //  Exists
         const updatedFavMeals = [...state.favoriteMeals];
@@ -24,6 +26,24 @@ const mealsReducer = (state = initialState, action) => {
         const meal = state.meals.find(meal => meal.id === action.mealId);
         return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
       }
+    case SET_FILTERS:
+      const appliedFilters = action.filters;
+      const updatedFilteredMeals = state.meals.filter(meal => {
+        if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+          return false;
+        }
+        if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+          return false;
+        }
+        if (appliedFilters.vegetarian && !meal.isVegetarian) {
+          return false;
+        }
+        if (appliedFilters.vegan && !meal.isVegan) {
+          return false;
+        }
+        return true;
+      });
+      return { ...state, filteredMeals: updatedFilteredMeals };
     default:
       return state;
   }
